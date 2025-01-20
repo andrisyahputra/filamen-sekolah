1e732a3cac89ba51692b0d58b074cdfc750da41f
Andri Syahputra <andrisyahputra2209@gmail.com>
2025-01-21 04:40:22 +0700

    update kernel

--- routes/Kernel.php
+++ routes/Kernel.php
@@ -4,16 +4,24 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
+
+    protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --stop-when-empty')
-            ->everyMinute()
-            ->withoutOverlapping();
+        $schedule->command('queue:work --tries=3')
+            ->cron('* * * * *')
+            ->withoutOverlapping(5)
+            ->before(function () {
+                Log::info('Queue worker started at: ' . now());
+            })
+            ->after(function () {
+                Log::info('Queue worker completed at: ' . now());
+            });
     }
 }
